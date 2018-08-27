f06889fad2c2f3898abc5ac28603c5bca8bd7d9e
Blak-Rex <42627934+Blak-Rex@users.noreply.github.com>
2018-08-27 11:54:12 -0400

    Add project files.

---
+++ Project2/Hello World Tutorial C++.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std; // Nos facilita la impresion en pantalla al no tener que indicar "std::cout" siempre.
+
+int main() 
+{
+	cout << "Hello World!";
+
+
+	return 0;
+}
